e99f8af438f4b32e23b0d96b6525af984fa91e34
Lalit Patidar <lalitpatidar1524@gmail.com>
2026-08-20 14:45:21 +0530

    fix: tolerate root hydration changes

--- apps/user/app/layout.tsx
+++ apps/user/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning className={`${manrope.variable} ${spaceGrotesk.variable}`}>
         <AppShellClient>{children}</AppShellClient>
       </body>
